700ab3c5e1a6b96f6449734c7b9dbc25ff5d90cc
magent88jp <magnet88jp@gmail.com>
2021-05-17 09:09:17 +0900

    rm dist

--- dist/_nuxt/static/1621160436/blog/what-we-know/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmagnet88jp-nuxt-blog\u002F_nuxt\u002Fstatic\u002F1621160436",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fwhat-we-know",config:{_app:{basePath:"\u002Fmagnet88jp-nuxt-blog\u002F",assetsPath:"\u002Fmagnet88jp-nuxt-blog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0febf165"}}}}(null));
