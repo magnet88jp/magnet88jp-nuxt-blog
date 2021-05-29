5e7f56171585146f1238222031fb1dd26e37afa2
magnet88jp <magnet88jp@users.noreply.github.com>
2021-05-29 12:11:46 +0000

    deploy: 60ee75b4188596df692ae4309a685e5acab70b83

--- _nuxt/static/1622278699/blog/my-third-blog-post/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmagnet88jp-nuxt-blog\u002F_nuxt\u002Fstatic\u002F1622278699",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fmy-third-blog-post",config:{_app:{basePath:"\u002Fmagnet88jp-nuxt-blog\u002F",assetsPath:"\u002Fmagnet88jp-nuxt-blog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"de3ca13b"}}}}(null));
